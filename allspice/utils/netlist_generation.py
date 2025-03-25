5b5734187b11da54c97460b2fd44876bc76a5473
Shrikanth Upadhayaya <shrik450@gmail.com>
2025-03-25 13:44:56 -0400

    netlist: Permissively handle component on PCB without designator

--- allspice/utils/netlist_generation.py
+++ allspice/utils/netlist_generation.py
@@ -82,6 +82,10 @@ def _extract_all_pcb_components(
     component_instances = get_all_pcb_components(repository, ref, pcb_file)
 
     for component in component_instances.values():
+        if "designator" not in component:
+            logger.warning("No component designator: component has no defined designator.")
+            continue
+
         pins = []
         for pin in component["pads"].values():
             try:
